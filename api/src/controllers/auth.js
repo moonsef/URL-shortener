6efa37aa63d7a8b2d33a5a53f21a67c9cc68c9ab
moonsef <bendriouichemonsef@gmail.com>
2022-06-01 12:16:43 +0100

    validate login request body

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -1,8 +1,20 @@
+const Joi = require("joi");
+const authRepo = require("../repository/auth");
 
-const login = (req, res) => {
-    
-}
+const login = async (req, res) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
 
-module.exports ={
-    login,
-}
+  const { error, value } = schema.validate(req.body);
+
+  if (error) {
+    res.status(400).json({ error: "validation_error" });
+    return;
+  }
+};
+
+module.exports = {
+  login,
+};
